1196ccfd5f1a2acb795e6af5daffe980732fdbcc
Mørch.Thomas Bowman TQM <TQM@kmd.dk>
2023-08-18 13:19:41 +0200

    Bump packages and fix eslint setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,131 +2,131 @@ import * as React from 'react'
 import MoEngine from './mo-engine'
 
 type Replacements = {
-	[key: string]: string | number,
+    [key: string]: string | number,
 }
 
 type TranslateProps = {
-	source: string,
-	sourcePlural?: string,
-	count?: number,
-	context?: string,
-	replacements?: Replacements,
-	domain?: string,
+    source: string,
+    sourcePlural?: string,
+    count?: number,
+    context?: string,
+    replacements?: Replacements,
+    domain?: string,
 }
 
 const replace = (input: string, replacements: Replacements) => {
-	let result = input
+    let result = input
 
-	const replacementKeys = Object.keys(replacements)
-	for (let i = 0; i < replacementKeys.length; i += 1) {
-		const from = replacementKeys[i]
-		const to = replacements[from]
-		result = result.split(`{${from}}`)
-			.join(`${to}`)
-	}
+    const replacementKeys = Object.keys(replacements)
+    for (let i = 0; i < replacementKeys.length; i += 1) {
+        const from = replacementKeys[i]
+        const to = replacements[from]
+        result = result.split(`{${from}}`)
+            .join(`${to}`)
+    }
 
-	return result
+    return result
 }
 
 export interface TranslateEngine {
-	translate(
-		source: string,
-		sourcePlural?: string,
-		count?: number,
-		context?: string,
-		textDomain?: string
-	): string;
+    translate(
+        source: string,
+        sourcePlural?: string,
+        count?: number,
+        context?: string,
+        textDomain?: string
+    ): string;
 }
 
 class MockTranslateEngine implements TranslateEngine {
-	// eslint-disable-next-line class-methods-use-this,no-unused-vars
-	translate(source: string, sourcePlural?: string, count?: number, context?: string) {
-		if (sourcePlural && count !== undefined && (count === 0 || count > 1)) {
-			return sourcePlural
-		}
+    // eslint-disable-next-line class-methods-use-this,no-unused-vars
+    translate(source: string, sourcePlural?: string, count?: number, context?: string) {
+        if (sourcePlural && count !== undefined && (count === 0 || count > 1)) {
+            return sourcePlural
+        }
 
-		return source
-	}
+        return source
+    }
 }
 
 export interface Translate {
-	_(
-		source: string,
-		context?: string,
-		replacements?: Replacements,
-		domain?: string,
-	): string,
-	_n(
-		singularSource: string,
-		pluralSource?: string,
-		count?: number,
-		context?: string,
-		replacements?: Replacements,
-		domain?: string,
-	): string,
+    _(
+        source: string,
+        context?: string,
+        replacements?: Replacements,
+        domain?: string,
+    ): string,
+    _n(
+        singularSource: string,
+        pluralSource?: string,
+        count?: number,
+        context?: string,
+        replacements?: Replacements,
+        domain?: string,
+    ): string,
 }
 
 export interface Translator extends Translate {
-	forDomain(domain: string): Translator
+    forDomain(domain: string): Translator
 }
 
 class TranslationProxy implements Translator {
-	engine: any;
+    engine: any
 
-	_perDomainCache: { [key: string]: TranslationProxy } = {
+    _perDomainCache: { [key: string]: TranslationProxy } = {
 
-	}
+    }
 
-	constructor(engine: TranslateEngine) {
-		this.engine = engine
-	}
+    constructor(engine: TranslateEngine) {
+        this.engine = engine
+    }
 
-	forDomain(domain: string) {
-		if (this._perDomainCache[domain] === undefined) {
-			const engine = {
-				translate: (
-					source: string,
-					sourcePlural?: string,
-					count?: number,
-					context?: string,
-					textDomain = domain,
-				) => (
-					this.engine.translate(source, sourcePlural, count, context, textDomain)
-				),
-			}
+    forDomain(domain: string) {
+        if (this._perDomainCache[domain] === undefined) {
+            const engine = {
+                translate: (
+                    source: string,
+                    sourcePlural?: string,
+                    count?: number,
+                    context?: string,
+                    textDomain = domain,
+                ) => (
+                    this.engine.translate(source, sourcePlural, count, context, textDomain)
+                ),
+            }
 
-			this._perDomainCache[domain] = new TranslationProxy(engine)
-		}
+            this._perDomainCache[domain] = new TranslationProxy(engine)
+        }
 
-		return this._perDomainCache[domain]
-	}
+        return this._perDomainCache[domain]
+    }
 
-	_ = (
-		source: string,
-		context?: string,
-		replacements: Replacements = {},
-		domain?: string,
-	) => this._n(source, undefined, undefined, context, replacements, domain)
+    _ = (
+        source: string,
+        context?: string,
+        replacements: Replacements = {},
+        domain?: string,
+    ) => this._n(source, undefined, undefined, context, replacements, domain)
 
-	_n = (
-		singularSource: string,
-		pluralSource?: string,
-		count?: number,
-		context?: string,
-		replacements: Replacements = {},
-		domain?: string,
-	) => replace(
-		this.engine.translate(
-			singularSource,
-			pluralSource,
-			count,
-			context,
-			domain,
-		),
-		replacements,
-	)
-		.replace(/%d/g, count !== undefined ? count.toFixed(0) : '')
-		.replace(/%f/g, count !== undefined ? count.toFixed(2) : '');
+    _n = (
+        singularSource: string,
+        pluralSource?: string,
+        count?: number,
+        context?: string,
+        replacements: Replacements = {},
+        domain?: string,
+    ) => replace(
+        this.engine.translate(
+            singularSource,
+            pluralSource,
+            count,
+            context,
+            domain,
+        ),
+        replacements,
+    )
+        .replace(/%d/g, count !== undefined ? count.toFixed(0) : '')
+        .replace(/%f/g, count !== undefined ? count.toFixed(2) : '')
 }
 
 const defaultValue: Translator = new TranslationProxy(new MockTranslateEngine())
@@ -136,131 +136,133 @@ TranslatorContext.displayName = 'TranslatorContext'
 export const TranslationContext: React.Consumer<Translator> = TranslatorContext.Consumer
 
 export function T(props: TranslateProps) {
-	return (
-		<TranslatorContext.Consumer>
-			{(engine: Translator) => {
-				const { count } = props
-				if (count === undefined || !props.sourcePlural) {
-					return engine._(
-						props.source,
-						props.context,
-						props.replacements,
-						props.domain,
-					)
-				}
+    return (
+        <TranslatorContext.Consumer>
+            {(engine: Translator) => {
+                const { count } = props
+                if (count === undefined || !props.sourcePlural) {
+                    return engine._(
+                        props.source,
+                        props.context,
+                        props.replacements,
+                        props.domain,
+                    )
+                }
 
-				return engine._n(
-					props.source,
-					props.sourcePlural,
-					count,
-					props.context,
-					props.replacements,
-					props.domain,
-				)
-			}}
-		</TranslatorContext.Consumer>
-	)
+                return engine._n(
+                    props.source,
+                    props.sourcePlural,
+                    count,
+                    props.context,
+                    props.replacements,
+                    props.domain,
+                )
+            }}
+        </TranslatorContext.Consumer>
+    )
 }
 
 type TranslationProviderProps = {
-	engine: TranslateEngine,
-	children: React.ReactNode,
+    engine: TranslateEngine,
+    children: React.ReactNode,
 }
 
 export function TranslationProvider({ engine, children }: TranslationProviderProps) {
-	return (
-		<TranslatorContext.Provider value={new TranslationProxy(engine)}>
-			{children}
-		</TranslatorContext.Provider>
-	)
+    const value = React.useMemo(() => new TranslationProxy(engine), [engine])
+
+    return (
+        <TranslatorContext.Provider value={value}>
+            {children}
+        </TranslatorContext.Provider>
+    )
 }
 
-export function withTextDomain<C>(domain: string) {
-	return (Component: React.ComponentType<C>) => function withDomain(props: C) {
-		return (
-			<TranslatorContext.Consumer>
-				{
-					engine => (
-						<TranslatorContext.Provider value={engine.forDomain(domain)}>
-							<Component {...props} />
-						</TranslatorContext.Provider>
-					)
-				}
-			</TranslatorContext.Consumer>
-		)
-	}
+export function withTextDomain<C extends Record<string, unknown>>(domain: string) {
+    return (Component: React.ComponentType<C>) => function withDomain(props: C) {
+        return (
+            <TranslatorContext.Consumer>
+                {
+                    (engine) => (
+                        <TranslatorContext.Provider value={engine.forDomain(domain)}>
+                            <Component {...props} />
+                        </TranslatorContext.Provider>
+                    )
+                }
+            </TranslatorContext.Consumer>
+        )
+    }
 }
 
 export function _(
-	source: string,
-	context?: string,
-	replacements: Replacements = {},
-	domain?: string,
+    source: string,
+    context?: string,
+    replacements: Replacements = {},
+    domain?: string,
 ): React.ReactElement<any> {
-	return (
-		<T
-			source={source}
-			context={context}
-			replacements={replacements}
-			domain={domain}
-		/>
-	)
+    return (
+        <T
+            source={source}
+            context={context}
+            replacements={replacements}
+            domain={domain}
+        />
+    )
 }
 
 export function _n(
-	singularSource: string,
-	pluralSource: string,
-	count: number,
-	context?: string,
-	replacements: Replacements = {},
-	domain?: string,
+    singularSource: string,
+    pluralSource: string,
+    count: number,
+    context?: string,
+    replacements: Replacements = {},
+    domain?: string,
 ): React.ReactElement<any> {
-	return (
-		<T
-			source={singularSource}
-			sourcePlural={pluralSource}
-			count={count}
-			context={context}
-			replacements={replacements}
-			domain={domain}
-		/>
-	)
+    return (
+        <T
+            source={singularSource}
+            sourcePlural={pluralSource}
+            count={count}
+            context={context}
+            replacements={replacements}
+            domain={domain}
+        />
+    )
 }
 
 export function useTranslate(domain?: string): Translate {
-	const contextValue = React.useContext(TranslatorContext)
+    const contextValue = React.useContext(TranslatorContext)
 
-	return React.useMemo(() => {
-		const singular = (
-			source: string,
-			context?: string,
-			replacements?: Replacements,
-			overrideDomain?: string,
-		) => contextValue._(source, context, replacements, overrideDomain || domain)
+    return React.useMemo(() => {
+        const singular = (
+            source: string,
+            context?: string,
+            replacements?: Replacements,
+            overrideDomain?: string,
+        ) => contextValue._(source, context, replacements, overrideDomain || domain)
 
-		const plural = (
-			singularSource: string,
-			pluralSource: string,
-			count: number,
-			context?: string,
-			replacements: Replacements = {},
-			overrideDomain?: string,
-		) => contextValue._n(
-			singularSource,
-			pluralSource,
-			count,
-			context,
-			replacements,
-			overrideDomain || domain,
-		)
+        const plural = (
+            singularSource: string,
+            pluralSource: string,
+            count: number,
+            context?: string,
+            replacements: Replacements = {},
+            overrideDomain?: string,
+        ) => contextValue._n(
+            singularSource,
+            pluralSource,
+            count,
+            context,
+            replacements,
+            overrideDomain || domain,
+        )
 
-		return {
-			_: singular,
-			_n: plural,
-		}
-	}, [contextValue, domain])
+        return {
+            _: singular,
+            _n: plural,
+        }
+    }, [contextValue, domain])
 }
 
 export {
-	MoEngine,
+    MoEngine,
 }
--- src/mo-engine.ts
+++ src/mo-engine.ts
@@ -1,38 +1,38 @@
-import Jed from 'jed';
-import jedGettextParser from 'jed-gettext-parser';
+import Jed from 'jed'
+import jedGettextParser from 'jed-gettext-parser'
 
 type DomainMap = {
   [key: string]: ArrayBuffer,
 }
 
 export default class MoEngine {
-    jed: any;
+    jed: any
 
     constructor(moData: ArrayBuffer, domain: string = 'messages', additionalDomains: DomainMap = {}) {
-      const domains = {
-        ...additionalDomains,
-        [domain]: moData,
-      };
+        const domains = {
+            ...additionalDomains,
+            [domain]: moData,
+        }
 
-      const localeData = Object.entries(domains)
-        .reduce((result, [name, data]) => ({
-          ...result,
-          ...jedGettextParser.mo.parse(data, { domain: name }),
-        }), {});
+        const localeData = Object.entries(domains)
+            .reduce((result, [name, data]) => ({
+                ...result,
+                ...jedGettextParser.mo.parse(data, { domain: name }),
+            }), {})
 
-      this.jed = new Jed({
-        locale_data: localeData,
-        domain: 'messages',
-      });
+        this.jed = new Jed({
+            locale_data: localeData,
+            domain: 'messages',
+        })
     }
 
     translate(
-      source: string,
-      sourcePlural?: string,
-      count?: number,
-      context?: string,
-      domain?: string,
+        source: string,
+        sourcePlural?: string,
+        count?: number,
+        context?: string,
+        domain?: string,
     ) {
-      return this.jed.dcnpgettext(domain, context, source, sourcePlural, count);
+        return this.jed.dcnpgettext(domain, context, source, sourcePlural, count)
     }
 }

b316e8d400f4a0faac01d351329fb08c631477c1
Thomas Mørch <tbowmo@users.noreply.github.com>
2023-08-21 16:50:06 +0200

    Update packages, fix eslint and setup github actions (#20)
    * Bump packages and fix eslint setup
    
    * Add github actions
    
    * Trying to fix test
    
    * Fix linting setup
    
    * Make tests work again
    
    ---------
    
    Co-authored-by: Mørch.Thomas Bowman TQM <TQM@kmd.dk>
    Co-authored-by: Wouter van Vliet <wouter@interpotential.dk>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,39 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb',
-  plugins: [
-  ],
-  rules: {
-    'react/require-default-props': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-indent-props': ['error', 'tab'],
-    'react/jsx-indent': ['error', 'tab'],
-    'no-underscore-dangle': 'off',
-    'semi': ['warn', 'never'],
-    'indent': ['error', 'tab'],
-    'no-tabs': ['error', {
-      allowIndentationTabs: true,
-    }],
-    'import/no-unresolved': {
-      peerDependencies: true,
+    parser: '@typescript-eslint/parser',
+    extends: 'airbnb',
+    plugins: [
+    ],
+    env: {
+        // Browser global variables like `window` etc.
+        browser: true,
+        // CommonJS global variables and CommonJS scoping. Allows require, exports and module.
+        commonjs: true,
+        // Enable all ECMAScript 6 features except for modules.
+        es6: true,
+        // Defines things like process.env when generating through node
+        node: true,
     },
-  },
-};
+    rules: {
+        'eol-last': 'error',
+        'default-param-last': 'off',
+        // Disabling 'import/no-unresolved' since typescript will break first
+        'import/no-unresolved': 'off',
+        'import/extensions': ['error', 'never'],
+        indent: ['warn', 4],
+        'linebreak-style': ['error', 'unix'],
+        'max-classes-per-file': 'off',
+        'no-console': ['warn'],
+        'no-underscore-dangle': 'off',
+        'no-unused-vars': 'off',
+        'no-use-before-define': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/jsx-indent-props': ['warn', 4],
+        'react/jsx-indent': ['warn', 4],
+        'react/jsx-props-no-spreading': 'off',
+        'react/require-default-props': 'off',
+        semi: ['warn', 'never'],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    },
+    root: true,
+}
---
+++ .mocharc.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'harmony': true,
+	'sort': true,
+	'exit': true, // This is a temporary workaround, until we figure out why mocha hangs
+}
